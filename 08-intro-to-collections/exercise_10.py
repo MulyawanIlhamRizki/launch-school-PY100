89b04d32341523f012cf7594ca1b588028c6f688
MulyawanIlhamRizki <mulyawanilhamrizki@gmail.com>
2024-04-21 21:34:30 +0800

    complete intro to collections exercises

---
+++ 08-intro-to-collections/exercise_10.py
@@ -0,0 +1,9 @@
+# Bob expects the following code to print the names in alphabetical order. 
+# Without running the code, can you say whether it will? Explain your answer.
+
+names = { 'Chris', 'Clare', 'Karis', 'Karl',
+          'Max', 'Nick', 'Victor' }
+print(names)
+
+# Since the above collection type is a set. It will not print alphebetically.
+# Sets are unordered, therefore it will not follow typical ordering.
---
+++ 08-intro-to-collections/exercise_11.py
@@ -0,0 +1,12 @@
+countries = {
+                "Alice" : "USA",
+                "Francois" : "Canada",
+                "Inti" : "Peru",
+                "Monika" : "Germany",
+                "Sanyu" : "Uganda",
+                "Yoshitaka" : "Japan",
+}
+
+print(type(countries))
+
+print(countries["Alice"]) # This should print USA in a dict key -> value pair
